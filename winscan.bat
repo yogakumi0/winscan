0c1c22d165cbe3af32c85d81db59ce772c85f40e
yogakumi <43279392+yogakumi0@users.noreply.github.com>
2024-05-05 16:36:27 +0200

    Add files via upload

--- winscan.bat
+++ winscan.bat
@@ -4,32 +4,41 @@ setlocal
 
 :: Run SFC (1)
 echo Scanning and repairing system files... (1)
+
+echo.
 sfc /scannow
+echo.
 
 :: Run DISM
 echo Repairing Windows image...
+
+echo.
 dism /online /cleanup-image /checkhealth
 dism /online /cleanup-image /scanhealth
 dism /online /cleanup-image /restorehealth
+echo.
 
 :: Ask the user if CHKDSK should be run
-set /p runChkdsk=Do you want to run CHKDSK? (Y/N): 
+set /p "runChkdsk=Do you want to run CHKDSK? (Y/N): "
 if /i "%runChkdsk%"=="Y" (
-    :: Ask the user for the drive letter
-    set /p driveLetter=Please enter the drive letter (If you dont know what letter, type C): 
-
-    :: Run CHKDSK for the selected drive
-    echo Scanning and repairing drive %driveLetter%...
-    chkdsk %driveLetter%: /f /x
+    echo.
+    echo Scanning and repairing drive C:
+    echo.
+    chkdsk C: /f /x
 ) else (
+    echo.
     echo CHKDSK skipped.
+    echo.
 )
 
 :: Run SFC (2)
 echo Scanning and repairing system files... (2)
+echo.
 sfc /scannow
+echo.
 
 echo Repair process completed.
+echo.
 pause
 
 endlocal
