cb74875c4b195b22151c0a562a3c97e0c37e93f4
yogakumi <lenard.hildenberg@icloud.com>
2024-05-06 20:28:42 +0200

    added some beatiful color, again

--- winscan.bat
+++ winscan.bat
@@ -20,8 +20,7 @@ setlocal
 
 :: Checking if the script is being run with administrator rights
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" && (
-    color 2F
-    echo Administrator privileges detected. Running script.
+    echo [92mAdministrator privileges detected. Running script.[0m 
     echo.
 ) || (
     color 4F
@@ -31,14 +30,14 @@ setlocal
 )
 
 :: Run SFC (1)
-echo Scanning and repairing system files... (1)
+echo ([92m1[0m) sfc
 
 echo.
 sfc /scannow
 echo.
 
 :: Run DISM
-echo Repairing Windows image...
+echo ([92m2[0m) dism
 
 echo.
 dism /online /cleanup-image /checkhealth
@@ -50,24 +49,24 @@ echo.
 set /p "runChkdsk=Do you want to run CHKDSK with repairing (needs to reboot your computer)? (Y/N): "
 if /i "%runChkdsk%"=="Y" (
     echo.
-    echo Scanning and repairing drive C:
+    echo ([92m3[0m) Scanning and repairing drive C:
     echo.
     chkdsk C: /f /x
 ) else (
     echo.
-    echo Scanning drive C:
+    echo ([92m3[0m) Scanning drive C:
     echo.
     chkdsk C:
     echo.
 )
 
 :: Run SFC (2)
-echo Scanning and repairing system files... (2)
+echo ([92m4[0m) sfc
 echo.
 sfc /scannow
 echo.
 
-echo Repair process completed.
+echo [92mRepair process completed.[0m
 echo.
 pause
 
